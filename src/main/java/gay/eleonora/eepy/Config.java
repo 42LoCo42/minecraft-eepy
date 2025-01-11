82ca8001cda3f86b973417c98b67f036829b8541
Leon Schumacher <leonsch@protonmail.com>
2025-01-11 21:13:51 +0100

    skip checks < X seconds after last player login

--- src/main/java/gay/eleonora/eepy/Config.java
+++ src/main/java/gay/eleonora/eepy/Config.java
@@ -2,11 +2,10 @@ package gay.eleonora.eepy;
 
 import net.minecraftforge.common.ForgeConfigSpec;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
-import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.Mod.EventBusSubscriber;
 import net.minecraftforge.fml.event.config.ModConfigEvent;
 
-@EventBusSubscriber(modid = Main.MODID, bus = Mod.EventBusSubscriber.Bus.MOD)
+@EventBusSubscriber(modid = Main.MODID, bus = EventBusSubscriber.Bus.MOD)
 public class Config {
 	private static final ForgeConfigSpec.Builder BUILDER = new ForgeConfigSpec.Builder();
 
@@ -14,12 +13,24 @@ public class Config {
 		.comment("Number of seconds to delay between checks")
 		.defineInRange("delaySeconds", 60, 1, Long.MAX_VALUE);
 
+	public static final ForgeConfigSpec.LongValue AFTER_LOGIN_SECONDS = BUILDER
+		.comment("If a check happens less than X seconds after a player login attempt, it's skipped")
+		.defineInRange("afterLoginSeconds", 60, 1, Long.MAX_VALUE);
+
+	public static final ForgeConfigSpec.LongValue AFTER_DEADLINE_SECONDS = BUILDER
+		.comment("If a check happens more than X seconds after its scheduled time, it's skipped")
+		.defineInRange("afterDeadlineSeconds", 1, 1, Long.MAX_VALUE);
+
 	static final ForgeConfigSpec SPEC = BUILDER.build();
 
 	public static long delaySeconds;
+	public static long afterLoginSeconds;
+	public static long afterDeadlineSeconds;
 
 	@SubscribeEvent
 	static void onLoad(final ModConfigEvent event) {
 		delaySeconds = DELAY_SECONDS.get();
+		afterLoginSeconds = AFTER_LOGIN_SECONDS.get();
+		afterDeadlineSeconds = AFTER_DEADLINE_SECONDS.get();
 	}
 }
--- src/main/java/gay/eleonora/eepy/Main.java
+++ src/main/java/gay/eleonora/eepy/Main.java
@@ -2,6 +2,7 @@ package gay.eleonora.eepy;
 
 import com.mojang.logging.LogUtils;
 import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.event.entity.player.PlayerNegotiationEvent;
 import net.minecraftforge.event.server.ServerStartedEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.ModLoadingContext;
@@ -16,26 +17,32 @@ import java.util.concurrent.TimeUnit;
 
 @Mod(Main.MODID)
 public class Main {
+	public static final long MS = 1000;
+
 	public static final String MODID = "eepy";
 	private static final Logger LOGGER = LogUtils.getLogger();
 
 	private static final ScheduledExecutorService scheduler = Executors.newScheduledThreadPool(1);
+
+	private long lastLogin = 0;
 
 	public Main() {
 		MinecraftForge.EVENT_BUS.register(this);
 		ModLoadingContext.get().registerConfig(ModConfig.Type.COMMON, Config.SPEC);
 	}
 
-	public Runnable mkTask(ServerStartedEvent event, long when) {
+	public Runnable mkTask(ServerStartedEvent event, long deadline) {
 		return () -> {
 			var now = System.currentTimeMillis();
-			if(now > when + 1000) {
-				LOGGER.warn("eepy check more than 1s too late, skipping");
+			if(now < lastLogin + Config.afterLoginSeconds * MS) {
+				LOGGER.debug("eepy check too early since last login, skipping");
+			} else if(now > deadline + Config.afterDeadlineSeconds * MS) {
+				LOGGER.debug("eepy check more than 1s too late, skipping");
 			} else {
-				LOGGER.info("running eepy check");
+				LOGGER.debug("running eepy check");
 
 				var players = event.getServer().getPlayerCount();
-				LOGGER.info("players online: {}", players);
+				LOGGER.debug("players online: {}", players);
 
 				if(players <= 0) {
 					LOGGER.info("the server is very eepy (＿ ＿*) Z z z");
@@ -52,7 +59,7 @@ public class Main {
 			}
 
 			scheduler.schedule(
-				mkTask(event, now + Config.delaySeconds * 1000),
+				mkTask(event, now + Config.delaySeconds * MS),
 				Config.delaySeconds, TimeUnit.SECONDS);
 		};
 	}
@@ -61,5 +68,10 @@ public class Main {
 	public void onServerStarted(final ServerStartedEvent event) {
 		LOGGER.info("eepy has started :3, delay is {} seconds", Config.delaySeconds);
 		scheduler.schedule(mkTask(event, System.currentTimeMillis()), 0, TimeUnit.SECONDS);
+	}
+
+	@SubscribeEvent
+	public void onPlayerNegotiation(final PlayerNegotiationEvent event) {
+		lastLogin = System.currentTimeMillis();
 	}
 }
